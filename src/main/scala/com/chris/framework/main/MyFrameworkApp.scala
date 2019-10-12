7c48026ebec9ae8b255176af23132d82b2b2b33b
jobs19951130 <964099604@qq.com>
2019-10-13 00:35:11 +0800

    add server start and listen requests

--- src/main/scala/com/chris/framework/main/MyFrameworkApp.scala
+++ src/main/scala/com/chris/framework/main/MyFrameworkApp.scala
@@ -1,7 +1,22 @@
 package com.chris.framework.main
 
-object MyFrameworkApp {
-  def startServer()={
+import java.net.ServerSocket
 
+import com.chris.framework.configuration.Config
+import com.chris.framework.util.{ActorPool, IOStream}
+
+object MyFrameworkApp {
+
+  def startServer()={
+    val server = new ServerSocket(Config().port)
+    val actor = ActorPool.getActor("actor")
+    while(true){
+      val socket = server.accept()
+      actor!IOStream(socket,socket.getInputStream,socket.getOutputStream)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    startServer()
   }
 }
