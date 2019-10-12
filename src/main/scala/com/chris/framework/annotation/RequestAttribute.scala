689343c73bcad6e1b6a0e5ee2dd1907fd7dbc316
jobs19951130 <964099604@qq.com>
2019-10-13 00:34:16 +0800

    change the parameters to parameter list

--- src/main/scala/com/chris/framework/annotation/RequestAttribute.scala
+++ src/main/scala/com/chris/framework/annotation/RequestAttribute.scala
@@ -2,4 +2,4 @@ package com.chris.framework.annotation
 
 import scala.annotation.StaticAnnotation
 
-class RequestAttribute(value:String) extends StaticAnnotation
+class RequestAttribute(value:List[String]) extends StaticAnnotation
