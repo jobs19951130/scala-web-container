02c85876fe7bb72410fcf3d14de3857291cf00c8
jobs19951130 <964099604@qq.com>
2019-10-13 17:45:49 +0800

    add get page

--- src/main/scala/com/chris/framework/util/DoAction.scala
+++ src/main/scala/com/chris/framework/util/DoAction.scala
@@ -1,7 +1,15 @@
 package com.chris.framework.util
 
+import java.io.{File, FileInputStream, FileNotFoundException, IOException, UnsupportedEncodingException}
 object DoAction {
 
+  def response(request:Request,response:Response)={
+    if(request.url.contains(".")){
+      getPage(request,response)
+    }
+    else
+      process(request,response)
+  }
   def process(request:Request,response:Response)={
     val actionName = request.url
     val prame:Map[String,String] = request.body
@@ -15,6 +23,29 @@ object DoAction {
       parameters=parameters.:+(p.substring(1,p.size-1))
     })
     response.print(methodName(parameters).toString)
+    response.push2Client(200)
+    response.closeIO()
+  }
+
+  def getPage(request:Request,response:Response)={
+    val pagePath = request.url
+    //this.getClass.getResourceAsStream("static/"+pagePath)
+    val file = new File(this.getClass.getResource("/static"+pagePath).getPath)
+    val fileLength = file.length
+    val fileContent = new Array[Byte](fileLength.intValue)
+    try {
+      val in = new FileInputStream(file)
+      in.read(fileContent)
+      in.close()
+    } catch {
+      case e: FileNotFoundException =>
+        e.printStackTrace()
+      case e: IOException =>
+        e.printStackTrace()
+    }
+    val str = new String(fileContent, "UTF-8")
+    println(str)
+    response.println(str)
     response.push2Client(200)
     response.closeIO()
   }
