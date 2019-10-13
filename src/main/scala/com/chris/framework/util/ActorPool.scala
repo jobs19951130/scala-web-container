0c356cbd36d6a41caa403ddffceb60a3ba5c8958
jobs19951130 <964099604@qq.com>
2019-10-13 19:48:49 +0800

    no message

--- src/main/scala/com/chris/framework/util/ActorPool.scala
+++ src/main/scala/com/chris/framework/util/ActorPool.scala
@@ -3,7 +3,6 @@ package com.chris.framework.util
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
 class ActorPool extends Actor{
-  // 重写接受消息的偏函数，其功能是接受消息并处理
   override def receive: Receive = {
     case IOStream(socket,inputStream,outputStream) =>
       val request = new WarpRequest(inputStream)
@@ -15,16 +14,15 @@ class ActorPool extends Actor{
       outputStream.close()
       request.close
       socket.close()
-    case "丑八怪" => println("滚犊子 ！")
     case "stop" => {
-      context.stop(self) // 停止自己的actorRef
-      context.system.terminate() // 关闭ActorSystem，即关闭其内部的线程池（ExcutorService）
+      context.stop(self)
+      context.system.terminate()
     }
   }
 }
 
 object ActorPool {
-  private val MyFactory = ActorSystem("myFactory")    //里面的"myFactory"参数为线程池的名称
+  private val MyFactory = ActorSystem("myFactory")
   def getActor(actorName:String):ActorRef={
     MyFactory.actorOf(Props[ActorPool], actorName)
 
